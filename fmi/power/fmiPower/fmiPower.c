109a8709fbcfcba3670fa6b2f9089a629c0899d8
Andris Pavenis <andris.pavenis@fmi.fi>
2019-09-25 12:58:00 +0300

    fmiPower: use watchdog chain to ensure taht process is running (reboot if not running)

--- fmi/power/fmiPower/fmiPower.c
+++ fmi/power/fmiPower/fmiPower.c
@@ -1,6 +1,8 @@
 #include "legato.h"
 #include "interfaces.h"
-#include <stdio.h>
+#include "watchdogChain.h"
+
+#define MS_WDOG_INTERVAL 8
 
 le_pm_WakeupSourceRef_t ws;
 
@@ -11,6 +13,11 @@ COMPONENT_INIT
   switch (le_pm_StayAwake(ws)) {
   case LE_OK:
     LE_INFO("Staying awake");
+
+    le_clk_Time_t watchdogInterval = { .sec = MS_WDOG_INTERVAL };
+    le_wdogChain_Init(1);
+    le_wdogChain_MonitorEventLoop(0, watchdogInterval);
+
     break;
   case LE_NO_MEMORY:
     LE_ERROR("Not enough memory for acquiring wakeup source");
