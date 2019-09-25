cb64abfd6cb2331b71b0d534e3f28e0d492e4738
Andris Pavenis <andris.pavenis@fmi.fi>
2019-09-25 12:56:53 +0300

    fmiDataConn: use watchdog chain to ensure that process is running

--- fmi/dataConn/fmiDataConn/fmiDataConn.c
+++ fmi/dataConn/fmiDataConn/fmiDataConn.c
@@ -1,6 +1,9 @@
 #include "legato.h"
 #include "interfaces.h"
 #include <stdio.h>
+#include "watchdogChain.h"
+
+#define MS_WDOG_INTERVAL 8
 
 le_data_RequestObjRef_t req;
 
@@ -19,4 +22,12 @@ COMPONENT_INIT
   le_data_AddConnectionStateHandler(&log_connection_state, NULL);
   le_data_ConnectService();
   req = le_data_Request();
+
+  // Try to kick a couple of times before each timeout.
+  // (borrowed from $LEGATO_ROOT/components/audio/le_audio.c)
+
+  le_clk_Time_t watchdogInterval = { .sec = MS_WDOG_INTERVAL };
+  le_wdogChain_Init(1);
+  le_wdogChain_MonitorEventLoop(0, watchdogInterval);
+
 }
